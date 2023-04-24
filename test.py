8ca4b3c994830ec3a638b4ad1524ab6fe80d970d
vpnnv <v.pnnv18@gmail.com>
2023-04-24 18:46:02 +0300

    Create test1.py

--- test.py
+++
@@ -1,9 +0,0 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = 100000
-deposit = []
-
-for key in per_cent:
-    deposit.append(per_cent[key] * money / 100)
-
-print(deposit)
-print("Максимальная сумма, которую вы можете заработать — {}".format(max(deposit)))
---
+++ test1.py
@@ -0,0 +1,4 @@
+def linear_solve(a,b):
+    return b/a
+
+print(linear_solve(0,1))
